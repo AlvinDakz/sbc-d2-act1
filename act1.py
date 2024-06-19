5dbacccd94b65abc6c1f066a4829627552075832
unknown <daquizalvin8@gmail.com>
2024-06-19 13:57:47 +0800

    version 3

---
+++ act1.py
@@ -0,0 +1,6 @@
+print(' this is a signle qoute')
+
+
+print(" this is a double qoute")
+
+print('143')
---
+++ sbc-d2-act1.py
@@ -0,0 +1,13 @@
+word = "summer bootcamp"
+
+
+print(word.title())
+
+print(word[0:14].capitalize() + word[-1].capitalize())
+
+print(word.replace("summer bootcamp", "Loot"))
+
+print(f"{word[-3].upper()}{word[-10].upper()}")
+
+wew = word.replace(" ", "")
+print(wew.isalpha())
