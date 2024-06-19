182f9d0e3544f3f9f3f82c40dbaf0f086654b60a
unknown <daquizalvin8@gmail.com>
2024-06-19 21:42:12 +0800

    sdc-d2-act2

---
+++ SBC-D2-ACT2.py
@@ -0,0 +1,33 @@
+from random import randint 
+while True:
+
+
+    p1 = input("Enter a Choice:")
+    c2 = randint(0,1)
+    c3 = randint(0,1)
+
+    choice = {
+        0: "FOLD",
+        1: "UNFOLD"
+    }
+
+    c2choice = choice[c2]
+    c3choice = choice[c3]
+
+    if p1 == "FOLD" and c2choice == "FOLD" and c3choice == "UNFOLD":
+        print(p1,c2choice,c3choice, "c3 wins")
+
+    elif p1 == "UNFOLD" and c2choice == "UNFOLD" and c3choice == "FOLD":
+        print(p1,c2choice,c3choice, "c3 wins")
+
+    elif p1 == "FOLD" and c2choice == "UNFOLD" and c3choice == "FOLD":
+        print("PLAYER - 1",p1,"c - 2",c2choice,"c3",c3choice,"P1 win")
+
+    elif p1 == "FOLD" and c2choice == "UNFOLD" and c3choice == "UNFOLD":
+        print("PLAYER - 1",p1,"c - 2",c2choice,"c3",c3choice,"P1 win")
+
+    else:
+        print("PLAYER - 1",p1,"c - 2",c2choice,"c3",c3choice,"draw")
+
+
+    
---
+++ sample.py
@@ -0,0 +1,34 @@
+#import random
+
+#num = (random.randint(0,100))
+
+#print(num)
+
+
+#lamp = False
+#plugged_in = True
+#burned_out = True
+
+#if lamp == plugged_in:
+#    if lamp == burned_out:
+#        print("Replace Lamp")
+#    else:
+#        print("Repair Lamp")
+#else:
+#    print("Plug in Lamp")
+
+
+age = int(input("Enter the your choice:"))
+
+if (age < 18):
+    print("Mapriso ka!")
+elif (age < 18):
+    print("Mapriso ka!")
+else:
+    print("PWede na")
+
+
+    
+    
+
+
